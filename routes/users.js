3616d4983c9307b5180bd1d1603b41a2101d23bd
juzer bhagat <juzerbhagat25@gmail.com>
2021-11-24 00:03:28 -0500

    Made user signup and login

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
+const { checkUser } = require('../data/users');
 const userData = data.users;
+
+router.get('/signup',async(req,res)=>{
+    res.render("users/signup");
+});
+
+router.get('/login',async(req,res)=>{
+    res.render("users/login");
+})
+
+router.post('/signup', async(req,res)=>{
+    userName = req.body.userName;
+    password = req.body.password;
+    fullName = req.body.fullName;
+    email = req.body.email;
+    dateOfBirth = req.body.dateOfBirth;
+    phoneNo = req.body.phoneNo;
+    address = req.body.address;
+    city = req.body.city;
+    state = req.body.state;
+    zipCode = req.body.zipCode;
+
+    try{
+        userCreated = await userData.createUser(userName,password,fullName,email,dateOfBirth,phoneNo,address,city,state,zipCode);
+        res.redirect('/login');
+    }
+    catch(e){
+        res.status(400).render("user/signup");
+    }
+});
+
+
+router.post('/login',async (req,res)=>{
+    let userName = req.body.userName;
+    let password = req.body.password;
+
+    try{
+        let user = await userData.checkUser(userName,password);
+        if(user.authenticated){
+            res.render("broadband/index");
+        }
+        
+    }catch(e){
+        res.status(400).render('users/login');
+    }
+
+});
 
 module.exports = router;
