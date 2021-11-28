faf8070e60dfcd4fdc86e0aec55f841ef161c516
Mehul Savaliya <mehulsavaliya520@gmail.com>
2021-11-28 11:19:44 -0500

    CSS

--- routes/broadband.js
+++ routes/broadband.js
@@ -3,45 +3,40 @@ const router = express.Router();
 const data = require('../data');
 const broadbandData = data.broadband;
 
-router.get('/', async (req,res) => {
-    if(req.session.user){
+router.get('/', async(req, res) => {
+    if (req.session.user) {
         let userName = req.session.user.userName;;
-        if(userName === "admin"){
-            res.render('broadband/index', {userName:userName, isAdmin:true});
+        if (userName === "admin") {
+            res.render('broadband/index', { userName: userName, isAdmin: true });
+        } else {
+            res.render('broadband/index', { userName: userName, isAdmin: false });
         }
-        else{
-            res.render('broadband/index', {userName:userName, isAdmin:false});
-        }
-    }
-    else{
+    } else {
         res.render('broadband/index');
     }
-    
+
 });
 
 
-router.get('/broadband/newPlan' , async(req,res) => {
-    if(!req.session.user){
+router.get('/broadband/newPlan', async(req, res) => {
+    if (!req.session.user) {
         res.redirect('/');
-    }
-    else{
-        if(req.session.user.userName === 'admin'){
-            res.render('broadband/newPlan', {userName: req.session.user.userName});
-        }
-        else{
+    } else {
+        if (req.session.user.userName === 'admin') {
+            res.render('broadband/newPlan', { userName: req.session.user.userName });
+        } else {
             res.redirect('/');
         }
     }
-    
+
 });
 
-router.get('/broadband/broadbandPlans', async (req,res)=> {
-    try{
+router.get('/broadband/broadbandPlans', async(req, res) => {
+    try {
         let broadbandList = await broadbandData.listPlans();
-        res.render('broadband/broadbandPlans', {broadbandList:broadbandList});
-    }   
-    catch(e){
-        res.status(500).json({error:e});
+        res.render('broadband/broadbandPlans', { broadbandList: broadbandList });
+    } catch (e) {
+        res.status(500).json({ error: e });
     }
 });
 
@@ -49,11 +44,6 @@ router.get('/broadband/broadbandPlans', async (req,res)=> {
 router.post('/broadband/insert', async(req, res) => {
 
     const broadbandPlans = req.body;
-    // planName = broadbandPlans.planName;
-    // price = broadbandPlans.price;
-    // validity = broadbandPlans.validity;
-    // limit = broadbandPlans.limit;
-    // discount = broadbandPlans.discount;
     try {
         const { planName, price, validity, limit, discount } = broadbandPlans;
         const newBroadband = await broadbandData.create(planName, price, validity, limit, discount);
