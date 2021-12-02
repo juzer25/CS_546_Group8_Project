f85ce4b7c02741ffa79bb6f6d74a6d0f007d252d
shivank <shivankgupta1969@gmail.com>
2021-12-01 22:29:08 -0500

    changes not stagged for commit

--- data/index.js
+++ data/index.js
@@ -1,11 +1,13 @@
 const userData = require('./users');
 const broadbandData = require('./broadband');
+const checkoutData = require('./checkout');
 const adminData = require('./admin');
 const appointmentData = require('./appointment');
 
 module.exports = {
     users : userData,
     broadband : broadbandData,
+    checkout : checkoutData,
     admin : adminData,
     appointment:appointmentData
 };
--- routes/checkout.js
+++ routes/checkout.js
@@ -3,15 +3,35 @@ const router = express.Router();
 const data = require('../data');
 const userCardData = data.checkout;
 
-
-router.post('/checkout', async(req, res) => {
-    let userName = req.body.userName;
-    let password = req.body.password;
-    console.log(userName);
-    try {
-        cardDetails = await userCardData.storeCardDetails(userId, nameOfCardHolder, cardNumber, expirationMonth, expirationYear);
-
-    } catch (e) {
-        res.status(400).render('/checkout');
-    }
+router.get('/checkout', async(req, res) => {
+    res.render("checkout/checkout")
 });
+
+
+router.get('/checkout/bill', async(req, res) => {
+    res.render("checkout/bill")
+});
+
+router.post('/payment', async(req, res) => {
+     let userName = req.body.Username;
+    //let userName = "shivank";
+    let nameOfCardHolder = req.body.cardname;
+    let cardNumber = req.body.cardnumber;
+    let expirationMonth = req.body.expmonth;
+    let expirationYear = req.body.expyear;
+    try{
+   // if (req.session.user) {
+
+       // let userName = req.session.user.userName;;
+
+        cardDetails = await userCardData.storeCardDetails(userName, nameOfCardHolder, cardNumber, expirationMonth, expirationYear);
+        res.redirect("/checkout/bill");
+    //}
+    }
+    catch(e){
+        res.status(500).json({ error: e });
+    }
+ 
+});
+
+module.exports = router;
