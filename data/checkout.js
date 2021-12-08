561685fe2da402abb4ee9d7744b12022192143f9
shivank <shivankgupta1969@gmail.com>
2021-12-08 07:08:14 -0500

    error handling for card details in data and routes files

--- data/checkout.js
+++ data/checkout.js
@@ -12,14 +12,92 @@ const exportedMethods = {
         if(!expirationMonth) throw "Please provide a expiration month";
         if(!expirationYear) throw "Please provide a expiration year";
 
+
+    if(nameOfCardHolder.length === 0){
+        throw "Please provide a card holder name";
+    }
+    if(nameOfCardHolder.trim().length === 0){
+        throw "Please provide a card holder name";
+    }
+
+
+    if(cardNumber.length === 0) {
+        throw "Please provide a card number";
+    }
+    if(cardNumber.trim().length === 0) {
+        throw "Please provide a card number";
+    }
+    if(cardNumber.length !== 16) {
+        throw"Please provide a valid card number";
+    }
+    if(typeof parseInt(cardNumber) !== 'number'){
+        throw "Card number should be Interger";
+    }
+
+
+    if(expirationMonth.length === 0) {
+        throw" Please provide a expiry Month";
+    }
+    if(expirationMonth.trim().length === 0) {
+        throw "Please provide a expiry Month";
+    }
+    if(expirationMonth.length !== 2 && expirationMonth.length !== 1){
+        throw "Please provide a valid expiry Month";
+    }
+    if(expirationMonth > 12){
+        throw "Please provide a valid expiry Month";
+    }
+    if(typeof parseInt(expirationMonth) !== 'number'){
+        throw "Expirt month should be Interger";
+    }
+
+
+    if(expirationYear.trim().length === 0) {
+        throw "Please provide a expiry year";
+    }
+    if(expirationYear.length !== 2) {
+        throw "Please provide a valid expiry year";
+    }
+    if(typeof parseInt(expirationYear) !== 'number'){
+        throw "Expiry year should be Interger";
+    }
+
+        //console.log(expirationYear);
+        let CurrentDate = new Date();
+        let currYearStr = CurrentDate.getFullYear().toString();
+        //let curr_date_str = CurrentDate.toString();
+        // console.log(CurrentDate);
+        // console.log(CurrentDate.getFullYear());
         
+        //let yearValidation = CurrentDate.getFullYear().toString();
+       // yearValidation = yearValidationtion.toString();
+        //console.log(yearValidation);
+        //yearValidation = yearValidation[2] + yearValidation[3];
+        let expirationYearFourDigit = "20";
+        
+        if(expirationYear.length === 2){
+            expirationYearFourDigit = expirationYearFourDigit + expirationYear;
+        }
+
+        // console.log(expirationYearFourDigit);
+        // console.log(currYearStr);
+        if(expirationYearFourDigit < currYearStr)
+            console.log("You card has been expired. Please use different card for payment");
+        else if(parseInt(expirationYearFourDigit) > parseInt(currYearStr) + 10){
+            console.log("Please use different card for payment. This card seems ambitious");
+        }
+        //console.log(parseInt(expirationYearFourDigit), parseInt(currYearStr) + 10);
+        // console.log(expirationYear);
+        // console.log(yearValidation);
+        //console.log(CurrentDate.getUTCFullYear());
+
         let cardDetails = {
             nameOfCardHolder:nameOfCardHolder,
             cardNumber:cardNumber,
             expirationMonth:expirationMonth,
             expirationYear:expirationYear
         }
-       // console.log(userName);
+        //console.log(userName);
         const userCollection = await users();
         const userData = await userCollection.findOne({ userName : userName });
 
--- routes/checkout.js
+++ routes/checkout.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
+const xss = require('xss');
 const userCardData = data.checkout;
 
 router.get('/checkout', async(req, res) => {
-    res.render("checkout/checkout")
+   // if (req.session.user) {
+        res.render("checkout/checkout")
+    // }
+    // else {
+    //     res.redirect('/');
+    // }
 });
 
 
@@ -13,24 +19,169 @@ router.get('/checkout/bill', async(req, res) => {
 });
 
 router.post('/payment', async(req, res) => {
-     let userName = req.body.Username;
-    //let userName = "shivank";
-    let nameOfCardHolder = req.body.cardname;
-    let cardNumber = req.body.cardnumber;
-    let expirationMonth = req.body.expmonth;
-    let expirationYear = req.body.expyear;
-    try{
-   // if (req.session.user) {
+    let userName = "shivank";
+    //let userName = xss(req.session.user.userName);
+    let nameOfCardHolder = xss(req.body.cardname);
+    let cardNumber = xss(req.body.cardnumber);
+    let expirationMonth = xss(req.body.expmonth);
+    let expirationYear = xss(req.body.expyear);
+    let cvv = xss(req.body.cvv)
+     
 
-       // let userName = req.session.user.userName;;
+    if(!userName) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a userName"});
+        return;
+    }
+    
+    if(!nameOfCardHolder) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a card holder name"});
+        return;
+    }
+    if(nameOfCardHolder.length === 0){
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a card holder name"});
+        return;
+    }
+    if(nameOfCardHolder.trim().length === 0){
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a card holder name"});
+        return;
+    }
 
-        cardDetails = await userCardData.storeCardDetails(userName, nameOfCardHolder, cardNumber, expirationMonth, expirationYear);
-        res.redirect("/checkout/bill");
-    //}
+    if(!cardNumber) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a card number"});
+        return;
     }
-    catch(e){
-        res.status(500).json({ error: e });
+    if(cardNumber.length === 0) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a card number"});
+        return;
     }
+    if(cardNumber.trim().length === 0) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a card number"});
+        return;
+    }
+    if(cardNumber.length !== 16) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a valid card number"});
+        return;
+    }
+    if(typeof parseInt(cardNumber) !== 'number'){
+        res.status(400).render("checkout/checkout",
+         {error:"Card number should be Interger"});
+        return;
+    }
+
+    if(!expirationMonth) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a expiry Month"});
+        return;
+    }
+    if(expirationMonth.length === 0) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a expiry Month"});
+        return;
+    }
+    if(expirationMonth.trim().length === 0) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a expiry Month"});
+        return;
+    }
+    if(expirationMonth.length !== 2 && expirationMonth.length !== 1){
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a valid expiry Month"});
+        return;
+    }
+    if(expirationMonth > 12){
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a valid expiry Month"});
+        return;
+    }
+    if(typeof parseInt(expirationMonth) !== 'number'){
+        res.status(400).render("checkout/checkout",
+         {error:"Expirt month should be Interger"});
+        return;
+    }
+
+    if(!expirationYear) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a expiry year"});
+        return;
+    }
+    if(expirationYear.trim().length === 0) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a expiry year"});
+        return;
+    }
+    if(expirationYear.length !== 2) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please provide a valid expiry year"});
+        return;
+    }
+    if(typeof parseInt(expirationYear) !== 'number'){
+        res.status(400).render("checkout/checkout",
+         {error:"Expiry year should be Interger"});
+        return;
+    }
+
+    let CurrentDate = new Date();
+    let currYearStr = CurrentDate.getFullYear().toString();
+    let expirationYearFourDigit = "20";
+        
+    if(expirationYear.length === 2){
+        expirationYearFourDigit = expirationYearFourDigit + expirationYear;
+    }
+    if(expirationYearFourDigit < currYearStr){
+        res.status(400).render("checkout/checkout",
+         {error:"You card has been expired. Please use different card for payment"});
+        return;
+    }
+    else if(parseInt(expirationYearFourDigit) > parseInt(currYearStr) + 10){
+        res.status(400).render("checkout/checkout",
+         {error:"Please use different card for payment. This card seems ambitious"});
+        return;
+    }
+    //TODO: Month expiry errorhandling is not perfect
+    if(!cvv) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please enter a cvv"});
+        return;
+    }
+    if(cvv.trim().length === 0) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please enter a cvv"});
+        return;
+    }
+    if(cvv.length > 3) {
+        res.status(400).render("checkout/checkout",
+         {error:"Please enter a cvv"});
+        return;
+    }
+    if(typeof parseInt(cvv) !== 'number'){
+        res.status(400).render("checkout/checkout",
+         {error:"CVV should be Interger"});
+        return;
+    }
+    //TODO: check if cvv should be added to database or not
+    
+       // if (req.session.user) {
+            try{
+            // let userName = req.session.user.userName;
+
+            cardDetails = await userCardData.storeCardDetails(userName, nameOfCardHolder, cardNumber, expirationMonth, expirationYear);
+            res.redirect("/checkout/bill");
+            }
+            catch(e){
+                res.status(500).json({ error: e });
+            }
+        // }else {
+        //     res.redirect('/');
+        // }
+    
  
 });
 
