0cc2068fc2d8afecc56a23c1a3df5ba9db999469
juzer bhagat <juzerbhagat25@gmail.com>
2021-12-08 21:17:16 -0500

    Fixed bug in url encoding

--- public/js/broadbandScript.js
+++ public/js/broadbandScript.js
@@ -5,6 +5,14 @@
     
     var plansTable = $('#plansTable'),
         userType = $('.userType');
+
+    function b2a(a) {
+            var c, d, e, f, g, h, i, j, o, b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", k = 0, l = 0, m = "", n = [];
+            if (!a) return a;
+            do c = a.charCodeAt(k++), d = a.charCodeAt(k++), e = a.charCodeAt(k++), j = c << 16 | d << 8 | e, 
+            f = 63 & j >> 18, g = 63 & j >> 12, h = 63 & j >> 6, i = 63 & j, n[l++] = b.charAt(f) + b.charAt(g) + b.charAt(h) + b.charAt(i); while (k < a.length);
+            return m = n.join(""), o = a.length % 3, (o ? m.slice(0, o - 3) :m) + "===".slice(o || 3);
+    }    
     
     let btnClicked = false;
     $(document).ready(function(){
@@ -31,7 +39,7 @@
             if(btnClicked){
                 //let hashed =  bcrypt.hash(btnClicked,saltRounds);
                 //console.log();
-                window.location = 'subscribe/'+col1+'?val='+btoa(''+btnClicked+'');;
+                window.location = 'subscribe/'+col1+'?val='+b2a(''+btnClicked+'');;
             }
             
         });
